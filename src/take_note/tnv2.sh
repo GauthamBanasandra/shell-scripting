c80eab6c449be8aa726430ff9c71283f0ab1810b
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-01-31 20:06:53 +0530

    Adding if statement to check if the user input exists.

--- src/take_note/tnv2.sh
+++ src/take_note/tnv2.sh
@@ -4,7 +4,13 @@ date=$(date)
 filename=note_$1.txt
 # Use the "read" command to read from stdin.
 # Usage read <stdout>
-read -p "Enter note: " note
-# Must enclose the $filename in double-quotes. Otherwise, it will throw an error if the filename contains spaces.
-echo "${date}: ${note}" >> "${filename}"
-echo "notes saved in ${filename}"
+read -p "enter note: " note
+
+# Checking if input exists.
+if [[ ${note} ]]; then
+	# Must enclose the $filename in double-quotes. Otherwise, it will throw an error if the filename contains spaces.
+	echo "${date}: ${note}" >> "${filename}"
+	echo "notes saved in ${filename}"
+else
+	echo "note was not saved"
+fi
